c8a74d527487f43999a0cb41a2af0d7532f26837
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-05-04 19:02:45 +0200

    added webflow devlink config to git

---
+++ .webflowrc.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    host: "https://api.webflow.com",
+    rootDir: "./devlink",
+    siteId: "5a770d8f4323220001d73352",
+    authToken: process.env.WF_AUTH_TOKEN,
+    cssModules: false
+};
