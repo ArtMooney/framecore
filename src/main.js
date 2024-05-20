fc9ef0a91049e529e7f4bfe4e68849da0deb9c44
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-05-20 21:58:05 +0200

    added tailwind

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from "vue";
 import "./css/global.css";
 import "./css/fonts.css";
+import "./css/tailwind.css";
 import App from "./App.vue";
 import createRouter from "./routes";
 
