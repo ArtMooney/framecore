990b5de1b79280967fe34e9ef606ed2a85c8d2dc
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-06-08 22:52:45 +0200

    more component separation and tailwind work

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,44 @@
 <script setup>
 import { Vue3Lottie } from "vue3-lottie";
+import NavbarLink from "../elements/NavbarLink.vue";
 import Cube from "../assets/Cube.vue";
 import MugHot from "../assets/MugHot.vue";
 import sandwich from "../assets/burger-menu.json";
 </script>
 
 <template>
-  <div id="navbar" class="content navbar">
-    <a href="/" class="navbar-logo"
-      ><img src="../assets/Framecore_color.svg" alt="" class="image contain"
+  <!--    <div id="navbar" class="content navbar">-->
+  <div
+    id="navbar"
+    class="relative z-10 mb-4 mt-0 grid grid-cols-2 items-stretch justify-stretch justify-items-stretch gap-4 overflow-visible pb-0 pt-4 sm:grid-cols-[0.5fr,1fr] md:grid-cols-[0.25fr,1fr]"
+  >
+    <!--    <a href="/" class="navbar-logo"-->
+    <a
+      href="/"
+      class="flex w-[86%] items-center transition-all duration-500 ease-in-out hover:m-2"
+      ><img
+        src="../assets/Framecore_color.svg"
+        alt=""
+        class="h-full w-full object-contain"
     /></a>
 
-    <Transition name="navbar-links">
-      <div v-if="mobileMenu" class="navbar-links">
-        <div class="navbar-arrow"></div>
-        <router-link class="navlink" :to="{ path: '/', hash: '#services' }">
-          <div>Tjänster</div>
-          <div class="navicon-wrapper">
-            <Cube class="navicon blur" />
-            <Cube class="navicon" />
-          </div>
-        </router-link>
-        <a href="/case" class="navlink">
-          <div>Kunder &amp; Case</div>
-          <div class="navicon-wrapper">
-            <MugHot class="navicon blur" />
-            <MugHot class="navicon" />
-          </div>
-        </a>
-        <a href="/contact" class="button border navbutton w-button"
-          >Kontakta oss</a
-        >
-      </div>
-    </Transition>
+    <!--    <Transition name="navbar-links">-->
+    <!--      <div v-if="mobileMenu" class="navbar-links">-->
+    <!--      class="col-auto grid h-full auto-cols-max auto-rows-auto grid-cols-1 content-end items-stretch justify-end gap-1"-->
+    <div
+      v-if="mobileMenu"
+      class="grid grid-flow-col content-end items-stretch justify-end gap-4 bg-transparent"
+    >
+      <NavbarLink path="/" hash="#services" :icon="Cube">Tjänster</NavbarLink>
+      <NavbarLink path="/case" hash="" :icon="MugHot"
+        >Kunder &amp; Case
+      </NavbarLink>
+
+      <a href="/contact" class="button navbutton w-button border"
+        >Kontakta oss</a
+      >
+    </div>
+    <!--    </Transition>-->
 
     <div class="navbar-sandwich large" @click="mobileMenu = !mobileMenu">
       <Vue3Lottie
@@ -94,19 +100,5 @@ export default {
 <style scoped>
 .navlink:hover .navicon.blur {
   opacity: 100%;
-}
-
-.navbar-links-enter-active,
-.navbar-links-leave-active {
-  transition:
-    transform 0.3s ease,
-    opacity 0.3s ease;
-  display: grid;
-}
-
-.navbar-links-enter-from,
-.navbar-links-leave-to {
-  transform: translate(0, 3rem);
-  opacity: 0;
 }
 </style>
