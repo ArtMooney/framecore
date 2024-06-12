fe791820d2772cc892840f83a3cf3d65ee2401e9
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-06-12 21:59:43 +0200

    tailwind on videoheading ready

--- src/components/VideoHeading.vue
+++ src/components/VideoHeading.vue
@@ -1,24 +1,24 @@
 <template>
   <div class="relative grid grid-cols-1 md:grid-cols-[0.25fr,1fr,0.25fr]">
-    <div class="html-embed w-embed">
-      <video
-        class="h-full w-full object-cover"
-        muted=""
-        playsinline=""
-        autoplay=""
-        loop=""
-      >
-        <source src="../assets/production%20ID_4374507.mp4" />
-      </video>
-    </div>
+    <video
+      class="col-start-1 col-end-4 row-start-1 row-end-1 h-full w-full items-stretch justify-items-stretch object-cover"
+      muted=""
+      playsinline=""
+      autoplay=""
+      loop=""
+    >
+      <source src="../assets/production%20ID_4374507.mp4" />
+    </video>
+
     <div class="absolute bottom-0 left-0 right-0 top-0 bg-black/25"></div>
+
     <div
       class="relative col-start-2 col-end-2 row-start-1 row-end-1 flex flex-col items-start justify-center p-8"
     >
       <h1 class="text-4xl md:text-5xl">
         Kreativa lösningar för webb och animation
       </h1>
-      
+
       <p class="pb-8 text-white">
         Vi erbjuder anpassade webb- och 3D-animationstjänster för att hjälpa
         ditt företag att skapa nytta. Kontakta oss idag för en kostnadsfri
