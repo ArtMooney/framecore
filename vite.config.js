788e04c197f246e6169d81a8638da26d59512b32
Henrik Skoglund <48514966+ArtMooney@users.noreply.github.com>
2024-06-14 17:20:37 +0200

    removed purge from config

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import VitePurgeCss from "vite-plugin-purgecss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VitePurgeCss()],
+  plugins: [vue()],
 });
