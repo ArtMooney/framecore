87448e8478909d7e6bb9af79decfba75e3daa555
Henrik Skoglund <henrik.skoglund@framecore.se>
2026-03-29 14:40:46 +0200

    removed unused code

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -2,30 +2,12 @@
   <div
     class="flex min-h-screen flex-col flex-nowrap items-stretch justify-between"
   >
-    <Navbar @navbarHeight="navbarHeight = $event" />
+    <Navbar />
     <div class="flex grow flex-col bg-neutral-900">
       <slot />
       <div class="mt-auto">
-        <Footer @footerHeight="footerHeight = $event" />
+        <Footer />
       </div>
     </div>
   </div>
 </template>
-
-<script>
-export default {
-  data() {
-    return {
-      navbarHeight: 0,
-      footerHeight: 0,
-    };
-  },
-
-  provide() {
-    return {
-      navbarHeight: () => this.navbarHeight,
-      footerHeight: () => this.footerHeight,
-    };
-  },
-};
-</script>
